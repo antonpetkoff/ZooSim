c958f6a6a6505142c5aaf3d8c5430e3daeeef8d4
tonynho <tonynho010@gmail.com>
2014-11-03 20:29:56 +0200

    add grow, eat, life_expectancy & more

--- animal.py
+++ animal.py
@@ -1,9 +1,35 @@
 
 class Animal:
+    DAYS_IN_MONTH = 30.0
+    MONTHS_IN_YEAR = 12.0
+    GROWTH_RATE_AGE = 30.0      # grows with 30 days per grow() call
+    GROWTH_RATE_WEIGHT = 1.0    # grows with 1 kilo per grow() call
 
     def __init__(self, species, age, name, gender, weight):
         self.species = species
-        self.age = age
+        self.age = age                  # stores days
         self.name = name
         self.gender = gender
         self.weight = weight
+
+        # call setters for the following variables
+        self.life_expectancy = None
+
+    def set_life_expectancy(self, life_expectancy):
+        self.life_expectancy = life_expectancy
+
+    def get_current_animal_year(self):
+        return int(self.age / self.DAYS_IN_MONTH / self.MONTHS_IN_YEAR) + 1
+
+    def get_chance_of_dying(self):
+        if self.life_expectancy is not None:
+            return self.get_current_animal_year() / self.life_expectancy
+        else:
+            raise ValueError("life_expectancy not set!")
+
+    def grow(self):
+        self.age += self.GROWTH_RATE_AGE
+        self.weight += self.GROWTH_RATE_WEIGHT
+
+    def eat(self):
+        self.weight += self.GROWTH_RATE_WEIGHT
--- animal_tests.py
+++ animal_tests.py
@@ -4,13 +4,42 @@ import unittest
 
 class TestAnimals(unittest.TestCase):
 
+    def setUp(self):
+        self.animal = Animal("Wolf", 1000, "Kumcho", "male", 60)
+
     def test_init(self):
-        animal = Animal("Tiger", 15, "Dingo", "male", 90)
+        animal = Animal("Tiger", 600, "Dingo", "male", 90)
         self.assertEqual(animal.species, "Tiger")
-        self.assertEqual(animal.age, 15)
+        self.assertEqual(animal.age, 600)
         self.assertEqual(animal.name, "Dingo")
         self.assertEqual(animal.gender, "male")
         self.assertEqual(animal.weight, 90)
+
+    def test_set_life_expectancy(self):
+        self.animal.set_life_expectancy(20)
+        self.assertEqual(self.animal.life_expectancy, 20)
+
+    def test_get_current_animal_year(self):
+        self.assertEqual(self.animal.get_current_animal_year(), 3)
+
+    def test_get_chance_of_dying_valid_life_expectancy(self):
+        self.animal.set_life_expectancy(20)
+        self.assertEqual(self.animal.get_chance_of_dying(), 0.15)
+
+    def test_get_chance_of_dying_value_error(self):
+        with self.assertRaises(ValueError):
+            self.animal.get_chance_of_dying()
+
+    def test_grow(self):
+        self.animal.grow()
+        self.assertEqual(self.animal.age, 1000 + self.animal.GROWTH_RATE_AGE)
+        self.assertEqual(self.animal.weight,
+                         60 + self.animal.GROWTH_RATE_WEIGHT)
+
+    def test_eat(self):
+        self.animal.eat()
+        self.assertEqual(self.animal.weight,
+                         60 + self.animal.GROWTH_RATE_WEIGHT)
 
 
 if __name__ == '__main__':
